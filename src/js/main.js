0229b129e7fd2f9602192fae52db66c212ca0f68
AlexRay-dev <61346034+AlexRay-dev@users.noreply.github.com>
2021-06-01 19:43:58 +0500

    added Map

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,57 @@
-/**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
+// /* Example-tabs */
+// const tabsExampleBtn = document.querySelectorAll(".map__item");
+// const tabsExampleItem = document.querySelectorAll(".map__tabs-item");
+
+// tabsExampleBtn.forEach(onTabBtnClick)
+
+// function onTabBtnClick(item) {
+//     item.addEventListener('click', function () {
+//         let currentBtn = item;
+//         let tabId = currentBtn.getAttribute("data-tab");
+//         let currentTab = document.querySelector(tabId)
+
+//         if (!currentBtn.classList.contains("map__item--active")) {
+//             tabsExampleBtn.forEach(function (item) {
+//                 item.classList.remove("map__item--active")
+//             });
+
+//             tabsExampleItem.forEach(function (item) {
+//                 item.classList.remove("map__tabs-item--active")
+//             });
+
+//             currentBtn.classList.add("map__item--active");
+//             currentTab.classList.add("map__tabs-item--active")
+//         };
+//     });
+// };
+// /* Example-tabs */
+
+
+
+/* Example-tabs */
+const tabsExampleBtn = document.querySelectorAll(".map__item");
+const tabsExampleItem = document.querySelectorAll(".map__tabs-item");
+
+tabsExampleBtn.forEach(onTabBtnClick)
+
+function onTabBtnClick(item) {
+    item.addEventListener('mouseover', function () {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId)
+
+
+            currentBtn.classList.add("map__item--active");
+            currentTab.classList.add("map__tabs-item--active")
+    });
+    item.addEventListener('mouseout', function () {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId)
+
+
+            currentBtn.classList.remove("map__item--active");
+            currentTab.classList.remove("map__tabs-item--active")
+    });
+};
+/* Example-tabs */
